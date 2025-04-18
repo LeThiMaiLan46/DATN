6ad0534768fc57eaa41b696b1d42b62a6e5e5856
Mai Lan <mailan21463@gmail.com>
2025-04-18 21:59:56 +0700

    update env

--- backend/src/routers/services_routers_vi.py
+++ backend/src/routers/services_routers_vi.py
@@ -24,18 +24,8 @@ tts_path_model = os.environ.get('VI_TTS_PATH_MODEL')
 tts_configs = os.environ.get('VI_TTS_CONFIGS')
 path_audio_stogare = os.environ.get('PATH_AUDIO_STOGARE')
 
-minio_access_key = os.environ.get('MINIO_ACCESS_KEY')
-minio_secret_key = os.environ.get('MINIO_SECRET_KEY')
-minio_hostname = os.environ.get('MINIO_HOSTNAME')
-minio_bucket_name= os.environ.get('MINIO_BUKET_NAME')
+path_audio_style = os.environ.get('AUDIO_SAMPLE_DIR')
 
-#====== __init__ services ============
-minio_workers = Minio_Worker(
-                                minio_hostname=minio_hostname,
-                                access_key=minio_access_key,
-                                secret_key=minio_secret_key,
-                                bucket_name=minio_bucket_name
-                            )
 
 tts_sevices = TTS_Vi_Services(
                             path_model=tts_path_model,
@@ -50,23 +40,16 @@ async def tts_transcribable(request:TTS_Request):
     text = request.text
     language = request.language
     audio_id = request.audio_id
-    audio_style = minio_workers.get_file_from_minio_server(
-                                                            path_audio_stogare= path_audio_stogare,
-                                                            original_name=audio_id,
-                                                            language = language
-                                                            )
-    if audio_style['status'] == 200:
-        path_audio_style = audio_style['audio_dir']
-        response = await tts_sevices.tts_transcribable(
-                                    text=text,
-                                    language=language,
-                                    path_audio_style=path_audio_style
-                                    )
-        with tempfile.NamedTemporaryFile(delete=False, suffix='.wav') as temp_file:
-            temp_file.write(response.getvalue())
-            temp_file_path = temp_file.name
-            # Return the WAV file as a FileResponse (equivalent to Flask's send_file)
-            return FileResponse(temp_file_path, media_type="audio/wav", filename="audio.wav")
-    else:
-        return audio_style
+
+
+    response = await tts_sevices.tts_transcribable(
+                                text=text,
+                                language=language,
+                                path_audio_style=path_audio_style
+                                )
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.wav') as temp_file:
+        temp_file.write(response.getvalue())
+        temp_file_path = temp_file.name
+        # Return the WAV file as a FileResponse (equivalent to Flask's send_file)
+        return FileResponse(temp_file_path, media_type="audio/wav", filename="audio.wav")
 
